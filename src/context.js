04e87e0c1aa9da9f0377c70b22d2062681724a5b
Masnoon Junaid <>
2021-01-16 19:06:41 +0530

    context.js configuration

---
+++ src/context.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+const BetContext = React.createContext()
+
+class BetProvider extends React.Component {
+
+  state = {
+    bets:[],
+  }
+
+  render(){
+
+    return(
+      <BetContext.Provider>
+        {this.props.children}
+      </BetContext.Provider>
+    )
+  }
+}
+
+const BetConsumer = BetContext.Consumer;
+export {BetProvider,BetConsumer}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BetProvider} from "./context"
+import {BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </BetProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
